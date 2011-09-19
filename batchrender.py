0ea0b63ca79c7343bf9389e06aee481950c9e012
Volker Haas <volker.haas@brainbot.com>
2011-09-19 15:56:31 +0200

    parallel fetching and rendering

--- batchrender.py
+++ batchrender.py
@@ -7,6 +7,9 @@
 import subprocess
 import os
 import py
+import time
+from multiprocessing import Process
+
 
 from config import Config
 
@@ -14,43 +17,98 @@ config = Config()
 
 log = py.log.Producer('batchrender')
 
+class Collection(object):
+
+    def __init__(self,
+                 config_url,
+                 collection_title,
+                 out_fn):
+        self.config_url = config_url.strip()
+        self.collection_title = collection_title.strip()
+        self.out_fn = os.path.join(config.output_basedir, out_fn.strip() + config.file_ext)
+        self.zip_fn = os.path.splitext(self.out_fn)[0] + '.zip'
+        self.fetch_log = os.path.splitext(self.out_fn)[0] + '_fetch.log'
+        self.render_log = os.path.splitext(self.out_fn)[0] + '_render.log'
+
 class BatchRender(object):
 
     def __init__(self):
         pass
 
     def run_cmd(self, cmd):
-        print 'cmd:', cmd
-        res = subprocess.call(cmd)
-        if not res:
-            print 'zip finished'
-        else:
-            print 'error', res
-        
+        p = subprocess.Popen(cmd,
+                             stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE)
 
-    def fetch(self, config_url, collection_title, zip_fn):
+        out, err = p.communicate()
+        if err:
+            log('ERROR command exited with error', err, cmd)
+            raise Exception
+
+
+    def fetch(self, collection):
         cmd = ['mw-zip',
-               '-c', config_url,
-               '--collectionpage', collection_title,
-               '-o', zip_fn
+               '-c', collection.config_url,
+               '--collectionpage', collection.collection_title,
+               '-l', collection.fetch_log,
+               '-o', collection.zip_fn,
+               '-x', # FIXME: noimages - speed up fetching while debugging
                ]
         self.run_cmd(cmd)
+        assert os.path.exists(collection.zip_fn), 'ERROR: no zip file after fetching'
 
-    def render(self, zip_fn, zim_fn, writer):
+    def render(self, collection):
         cmd = ['mw-render',
-               '-w', writer,
-               '-c', zip_fn,
-               '-o', zim_fn,
+               '-w', config.writer,
+               '-c', collection.zip_fn,
+               '-o', collection.out_fn,
+               '-l', collection.render_log,
             ]
         self.run_cmd(cmd)
+        assert os.path.exists(collection.out_fn), 'ERROR: no output file after rendering'
 
     def run(self):
-        for config_url, collection_title, out_fn in config.collection_list:
-            print '*'*40
-            out_fn = os.path.join(config.output_basedir, out_fn + config.file_ext)
-            zip_fn = os.path.splitext(out_fn)[0] + '.zip'
-            self.fetch(config_url, collection_title, zip_fn)
-            self.render(zip_fn, out_fn, config.writer)
+        fetch_queue = [Collection(*collection_info) for collection_info in config.collection_list if not collection_info[0].startswith('#')]
+        fetch_active = []
+
+        render_queue = []
+        render_active = []
+
+        pid2col = {}
+
+        while fetch_queue or fetch_active or render_queue or render_active:
+            # FETCH
+            for p in fetch_active:
+                if not p.is_alive():
+                    fetch_active.remove(p)
+                    log('fetching finished:', pid2col[p.pid].collection_title,'(', p.pid, 'exitcode', p.exitcode, ')')
+                    render_queue.append(pid2col[p.pid])
+            
+            while len(fetch_active) < config.max_parallel_fetch and fetch_queue:
+                collection = fetch_queue.pop()
+                p = Process(target=self.fetch, args=(collection,))
+                p.start()
+                fetch_active.append(p)
+                log('fetching started', collection.collection_title, '(', p.pid, ')')
+                pid2col[p.pid] = collection
+
+            # RENDER
+
+            for p in render_active:
+                if not p.is_alive():
+                    render_active.remove(p)
+                    log('rendering finished:', pid2col[p.pid].collection_title,'(', p.pid, 'exitcode', p.exitcode, ')')
+                
+            while len(render_active) < config.max_parallel_render and render_queue:
+                collection = render_queue.pop()
+                p = Process(target=self.render, args=(collection,))
+                p.start()
+                render_active.append(p)
+                log('rendering started', collection.collection_title, '(', p.pid, ')' )
+                pid2col[p.pid] = collection
+
+            time.sleep(5)
+            
 
 if __name__ == '__main__':
     br = BatchRender()
--- config.py
+++ config.py
@@ -15,7 +15,8 @@ class Config(object):
         self.collection_list_location = os.path.expanduser('batchrender_collections')
         self.output_basedir = os.path.expanduser('~/')
         self.writer = 'rl'
-        #update_frequency =
+        self.max_parallel_fetch = 1
+        self.max_parallel_render = 1
 
         self.readrc()
         self.collection_list = self.get_collection_list(self.collection_list_location)
@@ -34,7 +35,12 @@ class Config(object):
         for attr, val in cfg['main'].items():
             if hasattr(self, attr):
                 log('setting : %s=%s' % (attr, val))
-                setattr(self, attr, val)
+                if isinstance(getattr(self, attr), int):
+                    cast = lambda x: int(x)
+                else:
+                    cast = lambda x:x
+
+                setattr(self, attr, cast(val))
 
     def get_collection_list(self, fn):
         collection_list = []
@@ -44,8 +50,8 @@ class Config(object):
         return collection_list
 
     def get_output_extension(self):
-        ext_map = {'rl':'pdf',
-               'zim':'zim'
+        ext_map = {'rl':'.pdf',
+               'zim':'.zim'
             }
         assert self.writer in ext_map, 'ERROR: Invalid Writer'
         return ext_map[self.writer]
