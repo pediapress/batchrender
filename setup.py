512e26a8a515b0f514b2bd2440bc60b0ea104f0b
Ralf Schmitt <ralf@systemexit.de>
2012-03-15 18:16:47 +0100

    make python setup.py sdist work
    also,
    - up version to 0.1.1, move it to __init__.py
    - add MANIFEST.in/make-manifest.py
    - add a .gitignore file

--- setup.py
+++ setup.py
@@ -3,29 +3,21 @@
 # Copyright (c) 2011 PediaPress GmbH
 # See README.txt for additional licensing information.
 
-import os
-import ez_setup
-ez_setup.use_setuptools()
 from setuptools import setup
-import distutils.util
 
-version=None
-execfile(distutils.util.convert_path('batchrender/_version.py')) 
-# adds 'version' to local namespace
+def get_version():
+    d = {}
+    execfile("batchrender/__init__.py", d, d)
+    return d["__version__"]
+
 
 install_requires=['mwlib', 'py', 'argparse']
 
-def read_long_description():
-    fn = os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.txt")
-    return open(fn).read()
 
 def main():
-    # if os.path.exists(distutils.util.convert_path('Makefile')):
-    #     print 'Running make'
-    #     os.system('make')
     setup(
         name="mwlib.batchrender",
-        version=str(version),
+        version=get_version(),
         entry_points = {
             'console_scripts': ['batchrender = batchrender.batchrender:main'],
         },
@@ -35,11 +27,10 @@ def main():
         include_package_data=True,
         url = "http://code.pediapress.com/",
         description="batch render list of collections",
-        long_description = read_long_description(),
+        long_description = open("README.txt", "r").read(),
         license="BSD License",
         maintainer="pediapress.com",
-        maintainer_email="info@pediapress.com",
-    )
+        maintainer_email="info@pediapress.com")
 
 if __name__ == '__main__':
     main()
